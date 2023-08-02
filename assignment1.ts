da2d4df72266e69e06dc6bc323e5d79d5bc2a636
Abdul-Ahad-503 <141061678+Abdul-Ahad-503@users.noreply.github.com>
2023-08-02 13:41:49 +0500

    Add files via upload

---
+++ assignment1.ts
@@ -0,0 +1,53 @@
+//Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT."
+console.log("Hey Sir, I have started learning TYPESCRIPT.");
+console.log("\n");
+
+
+
+// - Store your name in a variable and print it.
+var nam:string = "Abdul Ahad";
+console.log(nam);
+
+
+//Store 10 numbers in different variables.
+var num1:number  = 3;
+var num2:number  = 4;
+var num3:number  = 6;
+var num4:number  = 45;
+var num5:number  = 94;
+var num6:number  = 23;
+var num7:number  = 23;
+var num8:number  = 1;
+var num9:number  = 4;
+var num10:number  = 8;
+console.log("\n");
+
+
+
+
+//Add all of them and print the SUM.
+var SUM:number = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+console.log("Sum of all numbers is: ", SUM);
+console.log("\n");
+
+
+
+
+//Print the Difference (subtraction).
+var Subt:number = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+console.log("Difference  of all numbers is: ", Subt);
+console.log("\n");
+
+
+
+
+//Print the result after multiplying all.
+var Prod:number = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+console.log("Product  of all numbers is: ", Prod);
+console.log("\n");
+
+
+
+//Take two numbers and print the division result.
+var divid:number = num6 / num1;
+console.log("division of two numbers is: ", divid);
